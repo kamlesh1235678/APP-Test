43f434a428d7bcfae5e51d977ac7d6a046d64a1b
kamlesh1235678 <kamlesh@taxila.in>
2025-04-19 16:00:13 +0530

    041912345678910

--- api/v1/module/admin/exam_controller.py
+++ api/v1/module/admin/exam_controller.py
@@ -260,4 +260,4 @@ class BatchWiseHallTicketAnnounce(APIView):
     def get(self, request , batch):
         announced =  HallTicketAnnounce.objects.filter(batch = batch , is_active = True)
         announced = HallTicketAnnounceListSerializer(announced , many = True)
-        return response_handler(message="hall ticket announced successfully" , code0 =200 , data = announced.data)
+        return response_handler(message="hall ticket announced successfully" , code =200 , data = announced.data)
